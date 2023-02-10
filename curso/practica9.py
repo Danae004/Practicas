834a586e2df3035ac35fc67221d5c98fa5f000cf
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-02-10 23:21:28 +0000

    revisar codigo

---
+++ curso/practica9.py
@@ -0,0 +1,12 @@
+"""
+Crear menú de la aplicación e importar palabras clave desde un fichero.
+"""
+
+def muestra_menu():
+    print('')
+    print('')
+    print('-------- Kwranking --------')
+    print('')
+    print('[1] – Importar palabras clave')
+    print('[2] – Mostrar palabras clave')
+    print('[0] – Salir')
